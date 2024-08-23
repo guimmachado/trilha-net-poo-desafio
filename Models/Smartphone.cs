87a50ae95093e67351eee5c98b950328980681c2
Guilherme Machado <guimiguel.machado@gmail.com>
2024-08-23 12:48:28 -0300

    Desafio resolvido

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -1,14 +1,20 @@
+using System.Security.Cryptography.X509Certificates;
+
 namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        public string Modelo { get; set; }
+        public string IMEI { get; set; }
+        public int MemoriaInterna { get; set; }
 
         public Smartphone(string numero)
         {
             Numero = numero;
             // TODO: Passar os parâmetros do construtor para as propriedades
+            MemoriaInterna = 16;
         }
 
         public void Ligar()
--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+namespace DesafioPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Nokia nokia = new Nokia("123456789");
+            nokia.InstalarAplicativo("WhatsApp");
+
+            Iphone iphone = new Iphone("987654321");
+            iphone.InstalarAplicativo("Instagram");
+        }
+    }
+}
